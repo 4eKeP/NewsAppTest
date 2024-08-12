82801eb1a7f44950cd464d81bb2313ec86e7585b
Egor <egor_chek@mail.ru>
2024-08-12 20:41:13 +0400

    добаленны описания

--- NewsAppTest/Services/NetworkClient/NetworkConstants.swift
+++ NewsAppTest/Services/NetworkClient/NetworkConstants.swift
@@ -6,14 +6,8 @@
 //
 
 import Foundation
-
+#warning("Здесь Нужно ввести свой NewsData API")
 enum NetworkConstants {
-    static let tokenValue = "6209b976-c7aa-4061-8574-573765a55e71"
-    static let tokenKey = "X-Practicum-Mobile-Token"
     static let baseURL = "https://newsdata.io"
-    static let acceptKey = "Accept"
-    static let acceptValue = "application/json"
-    static let contentTypeKey = "Content-Type"
-    static let contentTypeValue = "application/x-www-form-urlencoded"
-    static let apiKey = "pub_50547caf9f004837240fd7191eede47a43cff"
+    static let apiKey = "Здесь Нужно ввести свой NewsData API"
 }
